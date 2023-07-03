5cb4b0df8284b5b620d9884ed613ac276c98da47
Saad <mamin.bee16seecs@seecs.edu.pk>
2023-07-03 08:34:31 -0700

    Move my own code to a seperate file

---
+++ src/Core/Inc/common.h
@@ -0,0 +1,13 @@
+/*
+ * common.h
+ *
+ *  Created on: Jul 3, 2023
+ *      Author: msaad
+ */
+
+#ifndef INC_COMMON_H_
+#define INC_COMMON_H_
+
+void boot();
+
+#endif /* INC_COMMON_H_ */
---
+++ src/Core/Src/common.c
@@ -0,0 +1,15 @@
+/*
+ * common.c
+ *
+ *  Created on: Jul 3, 2023
+ *      Author: msaad
+ */
+
+
+#include "common.h"
+
+#include <stdio.h>
+
+void boot() {
+	puts("System boot!\r");
+}
