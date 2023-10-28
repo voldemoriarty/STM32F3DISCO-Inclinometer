e84f5c87d53f4b3312dc5182bbd8502e0e3adca4
Saad <mamin.bee16seecs@seecs.edu.pk>
2023-10-28 10:02:18 -0700

    create visual studio project for using VS as an editor

---
+++ STM32F3DISCO-Inclinometer/sys/stat.h
@@ -0,0 +1,94 @@
+
+#include <sys/types.h>
+
+#define _S_IFMT   0xF000 // File type mask
+#define _S_IFDIR  0x4000 // Directory
+#define _S_IFCHR  0x2000 // Character special
+#define _S_IFIFO  0x1000 // Pipe
+#define _S_IFREG  0x8000 // Regular
+#define _S_IREAD  0x0100 // Read permission, owner
+#define _S_IWRITE 0x0080 // Write permission, owner
+#define _S_IEXEC  0x0040 // Execute/search permission, owner
+
+#define S_IFMT   _S_IFMT
+#define S_IFDIR  _S_IFDIR
+#define S_IFCHR  _S_IFCHR
+#define S_IFREG  _S_IFREG
+#define S_IREAD  _S_IREAD
+#define S_IWRITE _S_IWRITE
+#define S_IEXEC  _S_IEXEC
+
+struct _stat32
+{
+    _dev_t         st_dev;
+    _ino_t         st_ino;
+    unsigned short st_mode;
+    short          st_nlink;
+    short          st_uid;
+    short          st_gid;
+    _dev_t         st_rdev;
+    _off_t         st_size;
+    __time32_t     st_atime;
+    __time32_t     st_mtime;
+    __time32_t     st_ctime;
+};
+
+struct _stat32i64
+{
+    _dev_t         st_dev;
+    _ino_t         st_ino;
+    unsigned short st_mode;
+    short          st_nlink;
+    short          st_uid;
+    short          st_gid;
+    _dev_t         st_rdev;
+    __int64        st_size;
+    __time32_t     st_atime;
+    __time32_t     st_mtime;
+    __time32_t     st_ctime;
+};
+
+struct _stat64i32
+{
+    _dev_t         st_dev;
+    _ino_t         st_ino;
+    unsigned short st_mode;
+    short          st_nlink;
+    short          st_uid;
+    short          st_gid;
+    _dev_t         st_rdev;
+    _off_t         st_size;
+    __time64_t     st_atime;
+    __time64_t     st_mtime;
+    __time64_t     st_ctime;
+};
+
+struct _stat64
+{
+    _dev_t         st_dev;
+    _ino_t         st_ino;
+    unsigned short st_mode;
+    short          st_nlink;
+    short          st_uid;
+    short          st_gid;
+    _dev_t         st_rdev;
+    __int64        st_size;
+    __time64_t     st_atime;
+    __time64_t     st_mtime;
+    __time64_t     st_ctime;
+};
+
+struct stat
+{
+    _dev_t         st_dev;
+    _ino_t         st_ino;
+    unsigned short st_mode;
+    short          st_nlink;
+    short          st_uid;
+    short          st_gid;
+    _dev_t         st_rdev;
+    _off_t         st_size;
+    time_t         st_atime;
+    time_t         st_mtime;
+    time_t         st_ctime;
+};
---
+++ STM32F3DISCO-Inclinometer/sys/time.h
@@ -0,0 +1,3 @@
+#ifdef environ
+#undef environ
+#endif
