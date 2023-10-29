8f04fee1a2bcb7699288e1a7ba09ede4f58126c1
Saad <mamin.bee16seecs@seecs.edu.pk>
2023-10-29 13:20:23 +0500

    reorder operations

--- src/MATLAB/live_data_plotter.m
+++ src/MATLAB/live_data_plotter.m
@@ -6,23 +6,30 @@ clearvars;
 serial_port  = 'COM3';
 baudrate     = 115200;
 preamble     = uint8([0x69, 0x42, 0x69, 0x42]);
-device       = serialport(serial_port, baudrate);
 
 h = figure(1);
 cfg = init_figure(h);
+
+% open serial port 
+device = serialport(serial_port, baudrate);
 
 % align data using preamble
 info('Syncing with device');
 sync_preamble(device, preamble);
 info('Synced');
 
+% read data 
 data = read_data(device);
 T0 = datetime;
+t = 0;
 
 while ishandle(h)
-    t = seconds(datetime - T0);
-    data = read_data_preamb(device);
+    % plot data
     update_figure(cfg, t, data);
+
+    % get new data from device
+    data = read_data_preamb(device);
+    t = seconds(datetime - T0);
 end
 
 info('Figure closed');
